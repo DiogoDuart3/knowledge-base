f433019fd60dc67658396b0ca8bdd27be37bed8e
Diogo Duarte <diogopcgamer123@gmail.com>
2019-05-30 16:15:51 +0100

    Design dos posts completo

--- routes/web.php
+++ routes/web.php
@@ -19,10 +19,14 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::group(['middleware' => 'auth'], function(){
-    Route::resource('/posts', 'PostController', ['except' => ['index']]);
-});
+//Route::group(['middleware' => 'auth'], function(){
+//    Route::resource('/posts', 'PostController', ['except' => ['index']]);
+//});
 
 Route::get('/posts', function(){
     return view('posts.index');
+});
+
+Route::get('/posts/1', function(){
+    return view('posts.show');
 });
