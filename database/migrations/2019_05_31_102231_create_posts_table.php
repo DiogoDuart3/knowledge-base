b3e76c29d6b17d09c3322815d6b3dda4deca97df
Diogo Duarte <diogopcgamer123@gmail.com>
2019-05-31 12:02:56 +0100

    Migrações das tables criadas

---
+++ database/migrations/2019_05_31_102231_create_posts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id')->unsigned();
+            $table->string('title');
+            $table->mediumText('body');
+            $table->unsignedBigInteger('answer_id');
+            $table->boolean('confirmed_by_admin');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
