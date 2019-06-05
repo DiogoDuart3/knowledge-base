b789a35e54a1e7d89951d7a4f7f04328d5c42d90
Diogo Duarte <diogoduart3@gmail.com>
2019-06-05 12:23:12 +0100

    Changed relationship between Post and Category from Many to Many to One to Many

--- app/models/Post.php
+++ app/models/Post.php
@@ -12,8 +12,8 @@ class Post extends Model
         return $this->belongsTo(User::class);
     }
 
-    public function categories(){
-        return $this->belongsToMany(Category::class);
+    public function category(){
+        return $this->belongsTo(Category::class);
     }
 
     public function comments(){
