fe8659dbd296c336cd4494346f61933a1641dcc9
Diogo Duarte <diogopcgamer123@gmail.com>
2019-06-25 12:57:30 +0100

    Added loading to "create new issue"

--- resources/views/issue/create.blade.php
+++ resources/views/issue/create.blade.php
@@ -11,51 +11,65 @@
            data-placement="top" title="Add mew issue"><i class="fa fa-check fa-2x"></i></a>
         <hr>
         @include('layouts.messages')
-        <div class="row">
-            <div class="col-12 mt-5">
-                {!! Form::model($issue, ['method'=>'POST', 'action'=>['IssueController@store'], 'id'=>'formCreate']) !!}
-                <div class="form-group row">
-                    <label for="subject" class="col-sm-2 col-form-label">Subject</label>
-                    <div class="col-sm-10">
-                        <input type="text" class="form-control" id="subject" name="subject" placeholder="Subject">
-                    </div>
+
+        <div class="row text-center" id="loading">
+            <div class="col-12">
+                <div class="spinner-border" role="status">
+                    <span class="sr-only">Loading...</span>
                 </div>
-                <div class="form-group row">
-                    <label for="description" class="col-sm-2 col-form-label">Description</label>
-                    <div class="col-sm-10">
-                        <textarea id="description" name="description" rows="10" cols="80"></textarea>
+            </div>
+            <div class="col-12 mt-2">
+                <h3>Loading...</h3>
+            </div>
+        </div>
+
+        <div class="row" id="loaded" style="display: none">
+            <div class="row">
+                <div class="col-12 mt-5">
+                    {!! Form::model($issue, ['method'=>'POST', 'action'=>['IssueController@store'], 'id'=>'formCreate']) !!}
+                    <div class="form-group row">
+                        <label for="subject" class="col-sm-2 col-form-label">Subject</label>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control" id="subject" name="subject" placeholder="Subject">
+                        </div>
                     </div>
-                </div>
-                <div class="form-group row">
-                    <label for="issue_solution" class="col-sm-2 col-form-label">Issue solution</label>
-                    <div class="col-sm-10">
-                        <textarea name="issue_solution" id="issue_solution" rows="10" cols="80"></textarea>
+                    <div class="form-group row">
+                        <label for="description" class="col-sm-2 col-form-label">Description</label>
+                        <div class="col-sm-10">
+                            <textarea id="description" name="description" rows="10" cols="80"></textarea>
+                        </div>
                     </div>
-                </div>
-                <div class="form-group row">
-                    <label class="col-sm-2 col-form-label">Category</label>
-                    <div class="col-sm-10">
-                        <div class="form-group">
-                            <select class="form-control" name="category_id">
-                                <option disabled selected>Selecione</option>
-                                @foreach($categories as $category)
-                                    <option value="{{ $category->id }}">{{ $category->name }}</option>
+                    <div class="form-group row">
+                        <label for="issue_solution" class="col-sm-2 col-form-label">Issue solution</label>
+                        <div class="col-sm-10">
+                            <textarea name="issue_solution" id="issue_solution" rows="10" cols="80"></textarea>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-sm-2 col-form-label">Category</label>
+                        <div class="col-sm-10">
+                            <div class="form-group">
+                                <select class="form-control" name="category_id">
+                                    <option disabled selected>Selecione</option>
+                                    @foreach($categories as $category)
+                                        <option value="{{ $category->id }}">{{ $category->name }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-sm-2" for="tags">Tags</label>
+                        <div class="col-sm-10">
+                            <select multiple class="form-control" name="tags[]">
+                                @foreach($tags as $tag)
+                                    <option value="{{ $tag->id }}">{{ $tag->name }}</option>
                                 @endforeach
                             </select>
                         </div>
                     </div>
+                    {!! Form::close() !!}
                 </div>
-                <div class="form-group row">
-                    <label class="col-sm-2" for="tags">Tags</label>
-                    <div class="col-sm-10">
-                        <select multiple class="form-control" name="tags[]">
-                            @foreach($tags as $tag)
-                                <option value="{{ $tag->id }}">{{ $tag->name }}</option>
-                            @endforeach
-                        </select>
-                    </div>
-                </div>
-                {!! Form::close() !!}
             </div>
         </div>
     </div>
@@ -87,5 +101,10 @@
         });
         CKFinder.setupCKEditor(description);
         CKFinder.setupCKEditor(issue_solution);
+
+        $(document).ready(function(){
+            $('#loading').css('display', 'none');
+            $('#loaded').css('display', 'flex');
+        });
     </script>
 @endsection
