61cbe10ddd6a4e6b8119a9ab63a1e84fcf1410ea
Diogo Duarte <diogopcgamer123@gmail.com>
2019-07-02 10:33:38 +0100

    Added console commands to create "tags" and "categories"

---
+++ app/Console/Commands/CreateCategory.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\models\Category;
+use Illuminate\Console\Command;
+
+class CreateCategory extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'make:category {name}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a new category';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $name = $this->argument('name');
+        Category::create(['name'=>$name]);
+        $this->info("Category $name created successfully");
+    }
+}
